da40d58722c15c7998ffdcb4e2042a03a30a96fa
Rylie <rylie.stone2005@gmail.com>
2024-02-15 09:32:05 -0500

    functionality to getting the cordinates completed

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,28 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 // Suggested initial states
 const initialMessage = ''
 const initialEmail = ''
 const initialSteps = 0
-const initialIndex = 4 // the index the "B" is at
+const initialIndex = 4 // the index the "B" is at 4
 
 export default function AppFunctional(props) {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
+  const [gridController, setGridController] = useState({
+    message: initialMessage,
+    email: initialEmail,
+    steps: initialSteps,
+    index: initialIndex
+  })
 
   function getXY() {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
+      let x = 0
+      let breaker = true
+      let ind = gridController.index
+      while (breaker) {
+        if (ind - 3 >= -2) {
+          x++
+          ind = ind - 3
+        } else {
+          breaker = false
+        }
+      }
+      let y = (gridController.index % 3) + 1
+      if (y === 1) y = y-1
+      return ({x: x, y: y})
   }
 
   function getXYMessage() {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
+    let xy = getXY()
+    return (`Coordiates (${xy.x}, ${xy.x})`)
   }
 
   function reset() {
     // Use this helper to reset all states to their initial values.
+    setGridController({
+      message: initialMessage,
+      email: initialEmail,
+      steps: initialSteps,
+      index: initialIndex
+    })
   }
 
   function getNextIndex(direction) {
@@ -47,7 +75,7 @@ export default function AppFunctional(props) {
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
+        <h3 id="coordinates">{getXYMessage()}</h3>
         <h3 id="steps">You moved 0 times</h3>
       </div>
       <div id="grid">
@@ -63,11 +91,11 @@ export default function AppFunctional(props) {
         <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={() => getNextIndex('left')}>LEFT</button>
+        <button id="up" onClick={() => getNextIndex('up')}>UP</button>
+        <button id="right" onClick={() => getNextIndex('right')}>RIGHT</button>
+        <button id="down" onClick={() => getNextIndex('down')}>DOWN</button>
+        <button id="reset" onClick={() => {getXY()}}>reset</button>
       </div>
       <form>
         <input id="email" type="email" placeholder="type email"></input>
